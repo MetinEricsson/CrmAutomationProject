501e505cc267d81d8e9efc241d4666de6104a43c
Metin Hasdemir <metin.hasdemir@consultant.turkcell.com.tr>
2020-01-20 20:56:23 +0300

    Initial methods was developed.

---
+++ src/main/java/Pages/MainPage.java
@@ -0,0 +1,46 @@
+package Pages;
+
+import BaseFiles.DriverManager;
+import BaseFiles.TestBase;
+import ExtendReport.ExtendReportMethods;
+import GeneralFiles.BasePage;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainPage extends BasePage {
+
+    private static By satisMenu_Btn= By.xpath("//a[@ng-reflect-router-link='/sales']/p");
+
+    ExtendReportMethods reporter=new ExtendReportMethods();
+
+    public MainPage openMainPage(){
+
+        try {
+            DriverManager.getDriver().navigate().to(TestBase.BASEURL);
+            DriverManager.getDriver().manage().timeouts().pageLoadTimeout(120L, TimeUnit.SECONDS);
+            reporter.Report_Info("Web sayfasi basarili bir sekilde acilmistir. URL: "+ TestBase.BASEURL);
+
+        }catch (Exception e){
+            reporter.Report_Fail("Sayfa acilamamistir! Detay: "+e.getMessage());
+        }
+        return this;
+    }
+
+    public MainPage satisMenuSec(){
+
+        try {
+            clickElement(satisMenu_Btn);
+            reporter.Report_Info("Menuden Satis tabi secilmistir.");
+        }catch (Exception e){
+            reporter.Report_Fail("Menu Satis tabinda hata alinmistir! Detay: "+e.getMessage());
+        }
+
+        return this;
+    }
+
+
+
+
+}
---
+++ src/main/java/Pages/SalePage.java
@@ -0,0 +1,128 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+
+public class SalePage extends MainPage {
+
+    private static By tckn_TxtBx= By.xpath("//input[@formcontrolname='tckn']");
+    private static By sonrakiAdim_MusteriSecimi_Btn= By.xpath("//button[@type='submit'][text()='Sonraki Adım']");
+    private static By sec_Btn=By.xpath("//button[text()='Seç']");
+    private static By sonrakiAdim_UrunSecimi_Btn= By.xpath("//span[@class='mat-button-wrapper'][text()='Sonraki Adım']");
+    private static By secimiIptalEt_GozdenGecir_Btn= By.xpath("//button[@class='btn btn-danger btn-sm btn-round'][text()='Seçimi İptal Et']");
+    private static By siparisiBaslat_Btn= By.xpath("//button/span[@class='mat-button-wrapper'][text()='Siparişi Başlat']");
+
+    public SalePage tcknGir(String identityNo){
+
+        try {
+
+            clickElement(tckn_TxtBx);
+            Thread.sleep(1000);
+            sendKeysToElement(tckn_TxtBx,identityNo);
+            reporter.Report_Info(identityNo+" TCKN olarak alana girilmistir.");
+
+        }catch (Exception e){
+            reporter.Report_Fail(e.getMessage());
+        }
+        return this;
+    }
+
+    public SalePage tcknAra(){
+        pressEnter();
+        checkPageIsReady();
+        return this;
+    }
+
+    private SalePage sonrakiAdimButonTikla(By locator){
+
+        try {
+
+            waitElementWithThreadSleep(locator,5);
+
+            clickElement(locator);
+            checkPageIsReady();
+            reporter.Report_Info("Sonraki Adim butonuna tiklanmistir.");
+
+        }catch (Exception e){
+            reporter.Report_Fail(e.getMessage());
+        }
+        return this;
+    }
+
+    public SalePage sonrakiAdimButonTikla_MusteriSecimi(){
+
+        sonrakiAdimButonTikla(sonrakiAdim_MusteriSecimi_Btn);
+        return this;
+    }
+
+    public SalePage sonrakiAdimButonTikla_UrunSecimi(){
+
+        sonrakiAdimButonTikla(sonrakiAdim_UrunSecimi_Btn);
+        return this;
+    }
+
+    public SalePage fiberPaketSec(int item){
+
+        try {
+
+            waitElementWithThreadSleepViaItem(sec_Btn,5);
+            int getNumberOfPacket= getSizeOfList(sec_Btn);
+            
+            if (!(getNumberOfPacket>=item)){
+                reporter.Report_Fail("Secilmek istenen urun sayisi mevcut urun sayisindan fazladir!"
+                + " Mevcut urun sayisi: "+getNumberOfPacket);
+            }else{
+
+                for (int i = 0; i <item ; i++) {
+
+                    clickElementInList(sec_Btn,0);
+                    Thread.sleep(1500);
+                }
+                
+            }
+        
+        }catch (Exception e){
+            reporter.Report_Fail(e.getMessage());
+        }
+        return this;
+    }
+
+    public SalePage urunKontrol_GozdenGecir(int numberOfChosenProduct){
+
+        try {
+
+            waitElementWithThreadSleepViaItem(secimiIptalEt_GozdenGecir_Btn,5);
+
+            int getChosenProductItem= getSizeOfList(secimiIptalEt_GozdenGecir_Btn);
+
+            if (getChosenProductItem==numberOfChosenProduct){
+                reporter.Report_Info("Secilen paketler ekran uzerine dogru yansimistir.");
+            }else{
+                reporter.Report_Fail("Secilen urunler ekrana eksik yansimistir!");
+            }
+
+        }catch (Exception e){
+            reporter.Report_Fail(e.getMessage());
+        }
+
+        return this;
+    }
+
+    public SalePage siparisiBaslatButonTikla(){
+
+        try {
+            waitElementWithThreadSleep(siparisiBaslat_Btn,5);
+            clickElement(siparisiBaslat_Btn);
+            reporter.Report_Info("Siparisi baslat butonuna tiklanmistir.");
+            checkPageIsReady();
+
+        }catch (Exception e){
+            reporter.Report_Fail(e.getMessage());
+        }
+        return this;
+    }
+
+
+
+
+}
